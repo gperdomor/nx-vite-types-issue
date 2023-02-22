4c20be6b2e1117722e66984c5e23aeeb520494dd
Gustavo Perdomo <gperdomor@gmail.com>
2023-02-22 16:00:23 -0300

    feat: button

--- packages/lib-rollup/src/index.ts
+++ packages/lib-rollup/src/index.ts
@@ -1,1 +1,12 @@
+export {
+  Button,
+  ButtonGroup,
+  ButtonGroupProps,
+  ButtonOptions,
+  ButtonProps,
+  ButtonSpinner,
+  IconButton,
+  IconButtonProps,
+  useButtonGroup,
+} from '@chakra-ui/react';
 export * from './lib/lib-rollup';
--- packages/lib-vite/src/index.ts
+++ packages/lib-vite/src/index.ts
@@ -1,1 +1,12 @@
+export {
+  Button,
+  ButtonGroup,
+  ButtonSpinner,
+  IconButton,
+  useButtonGroup,
+  type ButtonGroupProps,
+  type ButtonOptions,
+  type ButtonProps,
+  type IconButtonProps,
+} from '@chakra-ui/react';
 export * from './lib/lib-vite';
